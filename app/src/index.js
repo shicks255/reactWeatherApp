f8af8a435d7e9b511e2ae46bbdd50780a1e62580
shicks <shicks255@yahoo.com>
2018-05-28 16:55:40 -0400

    Initial commit

---
+++ app/src/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+
+class App extends React.Component {
+    constructor(props)
+    {
+        super(props);
+
+    }
+    render()
+    {
+        return (
+            <WeatherCard />
+        );
+    }
+}
+
+class WeatherCard extends React.Component {
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            daysOfTheWeek: [0, 1, 2, 3, 4, 5, 6],
+        }
+    }
+
+    render()
+    {
+        const days = this.state.daysOfTheWeek;
+
+        let allDays = days.map((step, day) =>
+        {
+
+            return ('');
+        });
+    }
+}
+
+class DayBox extends React.Component {
+    constructor(props)
+    {
+        super(props);
+    }
+    render()
+    {
+        return('this is a day box');
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
