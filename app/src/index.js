1571ec3a0a5dd7966143fd67db5363c5f383928e
shicks <shicks255@yahoo.com>
2018-05-28 20:38:15 -0400

    more stuff for the weather practice

--- app/src/index.js
+++ app/src/index.js
@@ -29,8 +29,8 @@ class WeatherCard extends React.Component {
         const days = this.state.daysOfTheWeek;
         let allDays = days.map((step, day) =>
         {
-            const div = <div className="dayBoxDiv" key={step}>Here</div>;
-            return (div);
+            const box = <DayBox dayOfWeek={step}/>
+            return (box);
         });
 
         return(allDays);
@@ -41,11 +41,36 @@ class DayBox extends React.Component {
     constructor(props)
     {
         super(props);
+        this.state = {
+            day: getDayOfWeek(props.dayOfWeek),
+            dayNumber: props.dayOfweek
+        }
     }
     render()
     {
-        return('this is a day box');
+        const thing = <div className="dayBoxDiv">
+            {this.state.day}
+        </div>
+        return thing;
     }
+}
+
+function getDayOfWeek(dayNumber)
+{
+    if (dayNumber === 0)
+        return "Sunday";
+    if (dayNumber === 1)
+        return "Monday";
+    if (dayNumber === 2)
+        return "Tuesday";
+    if (dayNumber === 3)
+        return "Wednesday";
+    if (dayNumber === 4)
+        return "Thursday";
+    if (dayNumber === 5)
+        return "Friday";
+    if (dayNumber === 6)
+        return "Saturday";
 }
 
 ReactDOM.render(
